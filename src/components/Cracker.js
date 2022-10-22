6e738def12209fe33ae5d7c873002a381819a2d2
Mukka Deepak <ramu92013@gmail.com>
2022-10-22 23:15:57 +0530

    feat: 💫 add animations

--- src/components/Cracker.js
+++ src/components/Cracker.js
@@ -1,25 +1,46 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import CrackerSVG1 from "../assets/3crackers.svg";
+import CrackerSVG2 from "../assets/4crackers.svg";
 
 const Cracker = () => {
-
-    const [animation,setAnimation]=useState('wiggle');
-    const DivRef=useRef()
-    useLayoutEffect(()=>{
-        DivRef.current.style.bottom='4rem'
-    },[animation])
+  const [animation, setAnimation] = useState("wiggle");
+  const RightRocket = useRef();
+  const LeftRockets = useRef();
+  useLayoutEffect(() => {
+    RightRocket.current.style.bottom = "3rem";
+    LeftRockets.current.style.bottom = "3rem";
+  }, [animation]);
 
   return (
-    <section className='flex flex-col items-center justify-center h-full bg-gray-300'>
-        <div ref={DivRef} className={`absolute bottom-4 h-24 w-24 bg-gray-800 text-white text-center animate-${animation} `}>
-            Cracker
-        </div>
-    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-    onClick={()=>{
-        setAnimation('moveup')
-    }}
-    >Fire up</button>
-    </section>
-  )
-}
+    <section className="flex flex-col items-center justify-center h-screen bg-gray-300">
+     
+      <img
+        alt="cracker"
+        src={CrackerSVG1}
+        ref={RightRocket}
+        className={`absolute bottom-4 h-52 w-52 text-white text-center animate-${animation} invisible`}
+      />
 
-export default Cracker
+    <img
+        alt="cracker"
+        src={CrackerSVG2}
+        ref={LeftRockets}
+        className={`absolute bottom-4 h-52 w-52 text-white text-center animate-${animation} invisible`}
+      />
+
+
+   
+      {animation==="wiggle" && <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={() => {
+          setAnimation("moveup");
+        }}
+      >
+        Fire up
+      </button>}
+    </section>
+  );
+};
+
+export default Cracker;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,59 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        keyframes: {
-          wiggle: {
-            '0%, 100%': { transform: 'rotate(-10deg)' },
-            '50%': { transform: 'rotate(10deg)' },
-          },
-          moveup:{
-           '0%':{
-              opacity:0.2,
-              bottom:'4rem'
-   
-          },
-          '25%':{bottom:'8rem'},
-          '50%':{bottom:'16rem'},
-          '75%':{bottom:'32rem'},
-           '100%':{  opacity:1,bottom:'64rem'}
-          }
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-10deg)" },
+          "50%": { transform: "rotate(10deg)" },
         },
-        animation: {
-          wiggle: 'wiggle 1s ease-in-out infinite',
-          moveup: 'moveup 2s ease-in infinite',
-        }
-
+        moveup: {
+          "0%": {
+            opacity: 0.4,
+            bottom: "3rem",
+            translate: "0 0",
+            rotate: "50deg",
+          },
+          "50%": {
+            opacity: 0.5,
+            visibility: "visible",
+            rotate: "55deg",
+        
+          },
+          "100%": {
+            opacity: 1,
+            bottom: "70rem",
+            visibility: "visible",
+            translate: "650px 650px",
+            rotate: "60deg",
+          },
+        },
+        moveleft: {
+          "0%": {
+            opacity: 0.4,
+            bottom: "3rem",
+            translate: "0 0",
+            rotate: "-50deg",
+          },
+          "50%": {
+            opacity: 0.5,
+            visibility: "visible",
+            rotate: "-55deg",
+        
+          },
+          "100%": {
+            opacity: 1,
+            bottom: "70rem",
+            visibility: "visible",
+            translate: "-350px -350px",
+            rotate: "-60deg",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+        moveup: "moveup 2s ease-in 3",
+        moveleft: "moveleft 2s ease-in 3",
+      },
     },
   },
   variants: {
