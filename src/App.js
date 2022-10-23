97ad34cf0be3d932c63cb1fcb30f8790fdec83fd
Mukka Deepak <ramu92013@gmail.com>
2022-10-23 19:39:46 +0530

    feat: 💫 update animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import Chakra from "./components/Chakra";
 import Cracker from "./components/Cracker";
+import ReactCanvasConfetti from 'react-confetti'
+import { useEffect, useState } from "react";
+// import useWindowSize from 'react-use/'
 
 function App() {
+  
+  const [confetti,setConfetti]=useState(false)
+
+  const isAnimated=(value)=>{
+       setConfetti(true)
+  }
+
+  const [dimensions,setDimensions]=useState({width:window.innerWidth,height:window.innerHeight})
+  
+  const showAnimation=()=>{
+    setDimensions({
+      width:window.innerWidth,height:window.innerHeight
+    })
+  }
+
+  useEffect(()=>{
+     window.addEventListener('resize',showAnimation)
+     return ()=>{
+      window.removeEventListener('resize',showAnimation)
+     }
+  },[dimensions])
+  // const [dimensions,]=useState({})
+
+
   return (
     <div className="App">
       <div className="w-full h-full bg-gray-900">
-      <Cracker/>
+      <Cracker isAnimated={isAnimated}/>
+      {/* <Chakra/> */}
+      { confetti && <ReactCanvasConfetti width={dimensions.width-10} height={dimensions.height-10} />}
       </div>
     </div>
   );
--- src/components/Cracker.js
+++ src/components/Cracker.js
@@ -3,11 +3,14 @@ import CrackerSVG1 from "../assets/3crackers.svg";
 import CrackerSVG2 from "../assets/4crackers.svg";
 import Message from "./Message";
 
-const Cracker = () => {
+const Cracker = ({isAnimated}) => {
   const [animation, setAnimation] = useState(false);
+
   const RightRocket = useRef();
   const LeftRockets = useRef();
+
   useLayoutEffect(() => {
+
     if(RightRocket.current && LeftRockets.current){
     RightRocket.current.style.bottom = "3rem";
     LeftRockets.current.style.bottom = "3rem";
@@ -47,15 +50,12 @@ const Cracker = () => {
       />
     }
       
-
-
-
-   
       {!animation && <button
         type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         onClick={() => {
           setAnimation(true);
+          isAnimated(true);
         }}
       >
         Fire up
